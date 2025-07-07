4faa72f2d974be24fd6febfe940a9d7e3de799d7
abdulrahman mohamed <abdullrhhmann@gmail.com>
2025-07-07 12:20:58 +0300

    fixing backend api

--- client/src/pages/CheckoutPage.js
+++ client/src/pages/CheckoutPage.js
@@ -126,7 +126,7 @@ const CheckoutPage = () => {
       })));
 
       // Send to backend
-      const response = await fetch('http://localhost:5000/api/orders', {
+      const response = await fetch('https://beyound.up.railway.app/api/orders', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(orderPayload)
