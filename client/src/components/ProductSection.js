a123bd858fcd1c38ea462d6c8ac2e6a922f42663
abdulrahman mohamed <abdullrhhmann@gmail.com>
2025-07-28 02:22:57 +0300

    Fixed product section

--- client/src/components/ProductSection.js
+++ client/src/components/ProductSection.js
@@ -1,5 +1,5 @@
 import React, { useRef as reactUseRef, useState, useEffect } from 'react';
-import { motion, AnimatePresence } from 'framer-motion';
+import { motion } from 'framer-motion';
 import { useInView } from 'framer-motion';
 import { useCart } from '../contexts/CartContext';
 import { useTranslation } from 'react-i18next';
